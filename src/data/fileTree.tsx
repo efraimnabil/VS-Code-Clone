0dddd1f2fdb207756c8c7ac79429c6da43d19510
Efraim Nabil <efraimefefraim@gmail.com>
2024-04-05 17:10:21 +0200

    Update fileTree.ts

--- src/data/fileTree.tsx
+++ src/data/fileTree.tsx
@@ -27,18 +27,56 @@ export const fileTree: IFile = {
               name: "Button.tsx",
               isFolder: false,
               content: `interface IProps {
-
+  text: string;
+  onClick: () => void;
 }
 
-const Button = ({}: IProps) => {
+const Button = ({ text, onClick }: IProps) => {
   return (
-    <button>Click me!</button>
+    <button onClick={onClick}>{text}</button>
   )
 }
 
 export default Button`,
             },
+            {
+              id: uuid(),
+              name: "App.tsx",
+              isFolder: false,
+              content: `import React from 'react';
+import Button from './Button';
+
+const App = () => {
+  const handleClick = () => {
+    console.log('Button clicked!');
+  }
+
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <Button text="Click me!" onClick={handleClick} />
+    </div>
+  );
+}
+
+export default App;`,
+            },
           ],
+        },
+        {
+          id: uuid(),
+          name: "index.tsx",
+          isFolder: false,
+          content: `import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);`,
         },
       ],
     },
@@ -67,6 +105,76 @@ export default Button`,
 `,
         },
       ],
+    },
+    {
+      id: uuid(),
+      name: "package.json",
+      isFolder: false,
+      content: `{
+  "name": "vite-react-ts",
+  "version": "1.0.0",
+  "scripts": {
+    "dev": "vite",
+    "build": "vite build",
+    "serve": "vite preview"
+  },
+  "dependencies": {
+    "react": "^17.0.0",
+    "react-dom": "^17.0.0"
+  },
+  "devDependencies": {
+    "@vitejs/plugin-react-refresh": "^1.3.1",
+    "@types/react": "^17.0.0",
+    "@types/react-dom": "^17.0.0",
+    "typescript": "^4.1.2",
+    "vite": "^2.0.0"
+  }
+}`,
+    },
+    {
+      id: uuid(),
+      name: "tsconfig.json",
+      isFolder: false,
+      content: `{
+  "compilerOptions": {
+    "target": "esnext",
+    "module": "esnext",
+    "strict": true,
+    "jsx": "preserve",
+    "importHelpers": true,
+    "moduleResolution": "node",
+    "experimentalDecorators": true,
+    "skipLibCheck": true,
+    "esModuleInterop": true,
+    "allowSyntheticDefaultImports": true,
+    "sourceMap": true,
+    "baseUrl": ".",
+    "types": [
+      "vite/client",
+      "jest"
+    ],
+    "paths": {
+      "@/*": [
+        "src/*"
+      ]
+    },
+    "lib": [
+      "DOM",
+      "DOM.Iterable",
+      "ESNext"
+    ]
+  },
+  "include": [
+    "src/**/*.ts",
+    "src/**/*.tsx",
+    "src/**/*.d.ts",
+    "tests/**/*.ts",
+    "tests/**/*.tsx"
+  ],
+  "exclude": [
+    "node_modules"
+  ]
+}`,
     },
   ],
 };
